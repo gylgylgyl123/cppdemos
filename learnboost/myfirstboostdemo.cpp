a1bd7ba514e06c59736e557c265835d73dbc3dc3
gylgylgyl123 <65530993+gylgylgyl123@users.noreply.github.com>
2021-12-30 20:11:49 +0800

    Update myfirstboostdemo.cpp

--- learnboost/myfirstboostdemo.cpp
+++ learnboost/myfirstboostdemo.cpp
@@ -13,11 +13,27 @@ int main()
     cout << s << endl;
 
     //但是有的情况不一定会成功,下面情况就会出错
-    int ii;
+    /*int ii;
     ii = lexical_cast<int>("abcd");
     cout << ii << endl;
-
+    */
+    //处理办法，使用异常来处理
+    int ii;
+    try
+    {
+        ii = lexical_cast<int>("abcd"); //会报一个boost::bad_lexical_cast的异常
+    }
+    catch (boost::bad_lexical_cast& e)
+    {
+        cout << e.what() << endl;//打印这个异常对象中的信息
+    }
 
     return 0;
 }
 
+/*
+输出结果为：
+123
+12.4
+bad lexical cast: source type value could not be interpreted as target
+*/
